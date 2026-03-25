4edceacda98e6044175774505f4530df5e3baa3a
Sarvesh Singh <sarveshsingh101@gmail.com>
2026-03-26 00:44:11 +0530

    tabufline: offset past dotnet solution explorer

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -15,10 +15,22 @@ M.base46 = {
 }
 
 -- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
--- }
+
+M.ui = {
+  tabufline = {
+    modules = {
+      treeOffset = function()
+        for _, win in ipairs(vim.api.nvim_list_wins()) do
+          local buf = vim.api.nvim_win_get_buf(win)
+          local ft  = vim.bo[buf].filetype
+          if ft == "dotnet_explorer" or ft == "NvimTree" then
+            return string.rep(" ", vim.api.nvim_win_get_width(win) + 1)
+          end
+        end
+        return ""
+      end,
+    },
+  },
+}
 
 return M
